150ef64aa81707f8b263e13b14faa591d7f89da2
DasunNethsara-04 <techsaralk.pro@gmail.com>
2023-03-06 07:00:42 -0800

    test update

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
             </div>
         </nav>
         <br/><br/>
-        <!-- <h1 class="display-1 align-middle text-center fnt">Welcome To<br>Digital Library Management System</h1> -->
+        <!-- <h1 class="display-1 align-middle text-center fnt">Digital Library Management System</h1> -->
         <h1 class="d-flex justify-content-center align-items-center flex-column text-center display-3 fnt">Welcome To<br>Digital Library Management System</h1>
 
         <!-- Carousel -->
