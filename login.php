cd58cbdf31833bb81b0a3ac41e6390d846635549
DasunNethsara-04 <techsaralk.pro@gmail.com>
2023-03-11 15:20:45 -0800

    Login Page finished

--- login.php
+++ login.php
@@ -17,6 +17,25 @@
         <div class="box-container">
             <div class="img-box">
                 <div class="fill">
+                    <div class="text-center inner-content"><br><br><br>
+                        <img src="Media/Richmond Colleg LOGO.png" alt="Richmond Colleg logo" class="logo"><br><br>
+                        <h2>Richmond College Student Portal Login</h2>
+                    </div>
+                    <!-- <br><br><br><br><br><br> -->
+                    <div class="container">
+                        <p title="About Richmond College" class="about">
+                            Richmond College is a primary and secondary school in Galle, Sri Lanka
+                            which was established as Galle High School in 1876. The founder of school
+                            was the Wesleyan Missionary George Bough. The first principal of the school
+                            was Rev. Samuel Langdon. In 1882 it was renamed Richmond College.
+                        </p>
+
+                        <div class="footer">
+                            <a href="#"><img src="Media/Facebook.png" alt="facebook icon" width="32">&nbsp;
+                                Find More About Us on Facebook</a>
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
@@ -26,9 +45,9 @@
                     <h6>Login to your account</h6>
                     <form action="" method="post" class="form">
                         <label for="username">Username</label><br>
-                        <input type="text" name="username" id="username" placehoder="Your Email" autocomplete="off"><br><br>
+                        <input type="text" name="username" id="username" placeholder="Your Username" autocomplete="off"><br><br>
                         <label for="password">Password</label><br>
-                        <input type="password" name="password" id="password" placehoder="Password" autocomplete="off"><br><br>
+                        <input type="password" name="password" id="password" placeholder="Your Password" autocomplete="off"><br><br>
                         <label for="role">Role</label><br>
                         <select>
                             <option value="0">-- Select who you are --</option>
@@ -36,7 +55,7 @@
                             <option value="Teacher">Teacher</option>
                             <option value="Student">Student</option>
                         </select>
-
+                        <br>
                         <button class="login" type="submit">Login</button>
 
                     </form>
