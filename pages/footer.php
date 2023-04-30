29124020296a31c33e972d7c3aa09e8f3f01e29c
DasunNethsara-04 <techsaralk.pro@gmail.com>
2023-04-30 14:55:05 -0700

    footer updated

--- pages/footer.php
+++ pages/footer.php
@@ -3,9 +3,9 @@
         <div class="d-flex align-items-center justify-content-between small">
             <div class="text-muted">Copyright &copy; RCTS <?php echo date("Y"); ?></div>
             <div>
-                <a href="http://www.tech.richmondcollege.lk/">RCTS</a>
+                <a href="http://www.tech.richmondcollege.lk/" title="Official website of Richmond College Technology Society">RCTS</a>
                 &middot;
-                <a href="http://www.richmondcollege.lk/">Richmond College</a>
+                <a href="http://www.richmondcollege.lk/" title="Official website of Richmond College">Richmond College</a>
             </div>
         </div>
     </div>
