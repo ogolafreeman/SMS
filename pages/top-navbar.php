982713b99d625696abbe141f93c31718ca094994
DasunNethsara-04 <techsaralk.pro@gmail.com>
2023-05-06 01:08:52 -0700

    Top Navigation bar updated

--- pages/top-navbar.php
+++ pages/top-navbar.php
@@ -1,6 +1,6 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="../../index.php">RCTS</a>
+    <a class="navbar-brand ps-3" href="../../index.php"><img src="../../Media/Richmond Colleg LOGO.png" width="45"> SMS</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
