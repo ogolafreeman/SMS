c7f9725deb207cc1112538dc578a1120ff395dcf
Dasun Nethsara <techsaralk.pro@gmail.com>
2023-09-12 23:12:04 +0530

    page UI Updated

--- index.php
+++ index.php
@@ -9,56 +9,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Management System</title>
-    <link rel="shortcut icon" href="Media/Richmond Colleg LOGO.png" type="image/x-icon">
+    <link rel="shortcut icon" href="https://img.freepik.com/free-vector/hand-drawn-pencil-high-school-logo_23-2149689302.jpg?w=900&t=st=1694527035~exp=1694527635~hmac=8e02c01f5752c7f5d7ff35f802f5c0943179870c4028ad3a81221ef9cc71300d" type="image/x-icon">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style_home.css">
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/preloader.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <title>Student Management System</title>
 </head>
 
 <body>
-    <div class="d-lg-flex half">
-        <div class="bg order-1 order-md-2" style="background-image: url('Media/kids.jpg'); background-position: center; background-size: cover;"></div>
-        <div class="contents order-2 order-md-1">
-            <div class="container">
-                <div class="row align-items-center justify-content-center">
-                    <div class="col-md-7">
-                        <h3>Login to your account</h3>
-                        <?php if (isset($_GET['error'])) { ?>
-                            <script>
-                                Swal.fire({
-                                    icon: 'warning',
-                                    title: 'Oops...',
-                                    text: "<?= $_GET['error'] ?>"
-                                })
-                            </script>
-                        <?php } ?>
-                        <p class="mb-4">Please log in to access your account and manage student information.
-                            Teachers can enter term test marks, while students can view their marks and track
-                            their academic progress.</p>
-                        <form action="data/login-data.php" method="post">
-                            <div class="form-group first">
-                                <label for="username">Username</label>
-                                <input type="text" class="form-control" placeholder="NIC or Admission No." id="username" required name="username" autocomplete="off">
-                            </div>
-                            <div class="form-group last mb-3">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control" placeholder="Your Password" id="password" required name="password" autocomplete="off">
-                            </div>
-                            <!-- <div class="d-flex mb-5 align-items-center">
-                                <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
-                                    <input type="checkbox" checked="checked" />
-                                    <div class="control__indicator"></div>
-                                </label>
-                                <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span>
-                            </div> -->
-                            <input type="submit" name="login" value="Log In" class="btn btn-block btn-primary">
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="preloader">
+    <div class="loading-content">
+      <img id="logo" src="./Media/logo.png" alt="logo">
+      <br><br><br>
+      <p style="background: transparent">Loading...</p>
+      <div class="loading">
+  
+      </div>
     </div>
+  </div>
+
+  <div class="login-container">
+  <?php if (isset($_GET['error'])) { ?>
+        <div class="alert alert-danger" role="alert">
+            <?= $_GET['error'] ?>
+        </div>
+    <?php } ?>
+  <img id="logo" src="./Media/logo.png" alt="logo"><br>
+  <h2>Student Management System</h2>
+      <br>
+    <form action="data/login-data.php" method="post">
+     
+      <input placeholder="NIC or Admission No." id="username" required name="username" type="text">
+      <input placeholder="Your Password" id="password" required name="password" type="password">
+
+      <button name="login" value="Log In" type="submit" id=submit>Log In</button>
+      <br><br>
+    </form>
+  </div>
+  
+  
+  <script>
+  
+    let timeOut = setTimeout(function() {
+      const preloader = document.querySelector('.preloader')
+      preloader.classList.add('preloader--hidden')
+    }, 3000);
+  </script>
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/popper.min.js"></script>
     <!-- <script src="js/bootstrap.min.js"></script> -->
